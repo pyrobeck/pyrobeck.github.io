0fd2bce29e7ec9f7f645b5740b11f6e8fa4e1efa
pyrobeck <87209578+pyrobeck@users.noreply.github.com>
2026-01-20 14:40:07 -0500

    name update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Portfolio from "./pages/portfolio";
 import Games from "./pages/games";
 import Contact from "./pages/contact";
