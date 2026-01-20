1725015040056421421f3583f9d4b73e54c7e462
pyrobeck <87209578+pyrobeck@users.noreply.github.com>
2026-01-20 17:27:04 -0500

    Update portfolio.jsx

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -315,7 +315,7 @@ export default function Portfolio() {
                 alt={project.title}
                 className="w-full h-[350px] object-cover transform transition-transform duration-300 group-hover:scale-105"
               />
-              <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity flex items-end p-4">
+              <div className="absolute inset-0 bg-linear-to-t from-black/70 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity flex items-end p-4">
                 <p className="text-sm font-medium text-white opacity-90">
                   {project.title}
                 </p>
